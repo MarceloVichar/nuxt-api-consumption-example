8f43994784de53e4f6a018d00682b0312fc61677
MarceloVichar <marcos.marcelo43@gmail.com>
2022-12-17 00:51:06 -0300

    added material-icons in project

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,8 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
     ]
   },
 
@@ -29,6 +30,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: './plugins/injectHelpers.js' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
