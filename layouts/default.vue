d69099fecbed28234b01c17e4ce6fe69d70cee8d
MarceloVichar <marcos.marcelo43@gmail.com>
2022-12-16 23:13:25 -0300

    added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-base-200 min-h-screen">
+    <div class="navbar bg-primary text-base-100 flex justify-between">
+      <div>ColorsTemplates</div>
+      <div>Welcome</div>
+    </div>
+    <div class="p-4">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout'
+}
+</script>
