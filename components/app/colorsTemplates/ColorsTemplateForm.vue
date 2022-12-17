e462e0ae450fee5ab8c33607c9be9fd417805dae
MarceloVichar <marcos.marcelo43@gmail.com>
2022-12-17 12:58:27 -0300

    added color-picker lib in project

---
+++ components/app/colorsTemplates/ColorsTemplateForm.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'ColorsTemplateForm'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/shared/ColorPicker.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="w-full input input-bordered flex items-center h-9">
+    <InputColorPicker
+      v-model="color"
+      class="!w-full !border-base-100"
+      @input="$emit('input', color === 'rgba(0, 0, 0, 0)' ? '#FFFFFF' : color)"
+      @change="$emit('input', color === 'rgba(0, 0, 0, 0)' ? '#FFFFFF' : color)"
+    />
+  </div>
+</template>
+
+<script>
+import InputColorPicker from 'vue-native-color-picker'
+export default {
+  name: 'ColorPicker',
+  components: {
+    InputColorPicker
+  },
+  props: {
+    value: {
+      type: String,
+      required: false,
+      default () {
+        return ''
+      }
+    }
+  },
+  data () {
+    return {
+      color: this.value
+    }
+  },
+  watch: {
+    value: {
+      handler (val) {
+        this.color = val
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.icp__input {
+  height: 20px !important;
+}
+</style>
