18bb268ce8625c99941caf97182fddc68c46b3aa
MarceloVichar <marcos.marcelo43@gmail.com>
2022-12-17 12:58:51 -0300

    finished create page

--- components/app/colorsTemplates/ColorsTemplateForm.vue
+++ components/app/colorsTemplates/ColorsTemplateForm.vue
@@ -1,13 +1,65 @@
 <template>
-
+  <form class="grid sm:grid-cols-2 gap-4" @submit.prevent="$emit('input', mutableForm)">
+    <div>
+      <label for="bg_color">Background Color</label>
+      <ColorPicker v-model="mutableForm.data.bg_color" />
+    </div>
+    <div>
+      <label for="bg_color">Text Color</label>
+      <ColorPicker v-model="mutableForm.data.text_color" />
+    </div>
+    <label class="cursor-pointer flex items-center gap-2">
+      <input v-model="isActive" type="checkbox" class="checkbox">
+      <span>Active</span>
+    </label>
+    <div class="col-span-2 flex justify-end">
+      <button type="submit" class="btn btn-success btn-outline">
+        Submit
+      </button>
+    </div>
+  </form>
 </template>
 
 <script>
+import ColorPicker from '@/components/shared/ColorPicker.vue'
+
 export default {
-  name: 'ColorsTemplateForm'
+  name: 'ColorsTemplateForm',
+  components: { ColorPicker },
+  props: {
+    value: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      isActive: false,
+      mutableForm: {
+        data: {
+          bg_color: '#ffffff',
+          text_color: '#ffffff',
+          active: 0
+        }
+      }
+    }
+  },
+  watch: {
+    isActive (val) {
+      val
+        ? this.mutableForm.data.active = 1
+        : this.mutableForm.data.active = 0
+    }
+  },
+  created () {
+    this.mutableForm = Object.assign({}, this.value)
+    this.setIsActive()
+  },
+  methods: {
+    setIsActive () {
+      if (this.mutableForm.data.active === 0) { this.isActive = false }
+      if (this.mutableForm.data.active === 1) { this.isActive = true }
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
--- pages/colors/create.vue
+++ pages/colors/create.vue
@@ -1,9 +1,46 @@
 <template>
-  <div>Criar</div>
+  <div>
+    <button class="btn btn-primary mb-4" @click="$router.go(-1)">
+      Back
+    </button>
+    <div class="m-auto w-full lg:max-w-3xl bg-base-100 p-4 shadow rounded">
+      <ColorsTemplateForm v-model="form" @input="create" />
+    </div>
+  </div>
 </template>
 
 <script>
+import ColorsTemplateForm from '@/components/app/colorsTemplates/ColorsTemplateForm.vue'
+
 export default {
-  name: 'CreateTemplate'
+  name: 'CreateTemplate',
+  components: { ColorsTemplateForm },
+  data () {
+    return {
+      form: {
+        data: {
+          bg_color: '#ffffff',
+          text_color: '#ffffff',
+          active: 0
+        }
+      }
+    }
+  },
+  methods: {
+    create () {
+      this.$axios.post('/api/admin/calendar_patterns', {
+        calendar_patterns: this.form.data
+      })
+        .then((res) => {
+          this.$toast.success(res.data.message)
+          this.$router.go(-1)
+        })
+        .catch((err) => {
+          Object.keys(err.response.data.message).forEach((error) => {
+            this.$toast.error(err.response.data.message[error][0], { position: 'bottom-right' })
+          })
+        })
+    }
+  }
 }
 </script>
